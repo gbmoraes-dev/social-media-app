ca6d35275c6034bfa796fa5644a236226df052d7
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-02 16:23:03 -0300

    feat: ✨ add authenticate controller

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,59 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+import { makeAuthenticateUseCase } from '../use-cases/factories/make-authenticate.usecase'
+
+import { InvalidCredentialsError } from '../use-cases/errors/invalid-credentials-error'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    username: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { username, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    const { user } = await authenticateUseCase.execute({
+      username,
+      password,
+    })
+
+    const token = await reply.jwtSign({
+      sign: {
+        sub: user.id,
+      },
+    })
+
+    const refreshToken = await reply.jwtSign({
+      sign: {
+        sub: user.id,
+        expiresIn: '7d',
+      },
+    })
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
