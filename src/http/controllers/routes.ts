5ea9db9861906a91e9765ba1388c2f576b3bca20
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-02 16:23:56 -0300

    feat: ✨ add authenticate in routes handler

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -2,7 +2,10 @@ import type { FastifyInstance } from 'fastify'
 
 import { register } from './register.controller'
 
+import { authenticate } from './authenticate.controller'
+
 export async function routes(app: FastifyInstance) {
   /* users routes */
   app.post('/register', register)
+  app.post('/authenticate', authenticate)
 }
