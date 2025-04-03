0ee7bb3f648fc965172a26ba962efeead8c43c91
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-03 00:55:29 -0300

    test: 🧪 add end-to-end tests for refresh token

---
+++ src/http/controllers/refresh.spec.ts
@@ -0,0 +1,66 @@
+import request from 'supertest'
+
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from 'vitest'
+
+import { sql } from 'drizzle-orm'
+
+import { app } from '@/app'
+
+import { db } from '@/db'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  beforeEach(async () => {
+    await db.execute(sql`BEGIN`)
+  })
+
+  afterEach(async () => {
+    await db.execute(sql`ROLLBACK`)
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server).post('/register').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      username: 'johndoe',
+      email: 'johndoe@test.com',
+      passwordHash: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/authenticate').send({
+      username: 'johndoe',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies || [])
+      .send()
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
