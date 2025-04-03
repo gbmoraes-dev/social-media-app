9f71b162e1f5a4d3eb8552bb79d6e03482312a52
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-03 00:01:52 -0300

    feat: ✨ add refresh in routes handler

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -4,8 +4,11 @@ import { register } from './register.controller'
 
 import { authenticate } from './authenticate.controller'
 
+import { refresh } from './refresh.controller'
+
 export async function routes(app: FastifyInstance) {
   /* users routes */
   app.post('/register', register)
   app.post('/authenticate', authenticate)
+  app.patch('/token/refresh', refresh)
 }
