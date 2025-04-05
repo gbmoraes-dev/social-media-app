f6d920dc988da58e91be7fe6d07c05b28c83ec1e
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-05 17:05:10 -0300

    feat: ✨ add create posts in routes handler

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -1,14 +1,21 @@
 import type { FastifyInstance } from 'fastify'
 
+import { verifyJwt } from '../middlewares/verify-jwt'
+
 import { register } from './register.controller'
 
 import { authenticate } from './authenticate.controller'
 
 import { refresh } from './refresh.controller'
 
+import { createPost } from './create-post.controller'
+
 export async function routes(app: FastifyInstance) {
   /* users routes */
   app.post('/register', register)
   app.post('/authenticate', authenticate)
   app.patch('/token/refresh', refresh)
+
+  /* posts routes */
+  app.post('/posts', { onRequest: [verifyJwt] }, createPost)
 }
